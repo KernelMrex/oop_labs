b9e5ff6b2d7d815685f87d80d6423baecfb3479e
Kernel Mrex <24671528+KernelMrex@users.noreply.github.com>
2020-02-18 12:17:08 +0300

    Fixed: formatting

--- lab1_radix/main.cpp
+++ lab1_radix/main.cpp
@@ -22,102 +22,102 @@ std::string IntToString(long n, uint8_t radix);
 
 /* Main logic */
 struct Args {
-	std::uint8_t sourceNotation;
-	std::uint8_t destinationNotation;
-	std::string value;
+    std::uint8_t sourceNotation;
+    std::uint8_t destinationNotation;
+    std::string value;
 };
 
 std::optional<Args> ParseArgs(int argc, char *argv[]) {
-	// Validate input values
-	if (argc != 4) {
-		return std::nullopt;
-	}
+    // Validate input values
+    if (argc != 4) {
+        return std::nullopt;
+    }
 
-	long sourceNotation = StringToInt(argv[1], 10);
-	if ((sourceNotation > MAX_RADIX) || (sourceNotation < MIN_RADIX)) {
-		throw std::runtime_error("bad range for source notation");
-	}
+    long sourceNotation = StringToInt(argv[1], 10);
+    if ((sourceNotation > MAX_RADIX) || (sourceNotation < MIN_RADIX)) {
+        throw std::runtime_error("bad range for source notation");
+    }
 
-	long destinationNotation = StringToInt(argv[2], 10);
-	if ((destinationNotation > MAX_RADIX) || (destinationNotation < MIN_RADIX)) {
-		throw std::runtime_error("bad range for destination notation");
-	}
+    long destinationNotation = StringToInt(argv[2], 10);
+    if ((destinationNotation > MAX_RADIX) || (destinationNotation < MIN_RADIX)) {
+        throw std::runtime_error("bad range for destination notation");
+    }
 
-	return {
-		(Args) {
-			static_cast<uint8_t>(sourceNotation),
-			static_cast<uint8_t>(destinationNotation),
-			argv[3]
-		}
-	};
+    return {
+            (Args) {
+                    static_cast<uint8_t>(sourceNotation),
+                    static_cast<uint8_t>(destinationNotation),
+                    argv[3]
+            }
+    };
 }
 
 int main(int argc, char *argv[]) {
-	try {
-		std::optional<Args> result = ParseArgs(argc, argv);
-		if (!result.has_value()) {
-			std::cout << "Wrong amount of parameters" << std::endl;
-			std::cout << "Usage: radix <source notation> <destination notation> <value>" << std::endl;
-			return 1;
-		}
-		long decimalNumberView = StringToInt(result->value, result->sourceNotation);
+    try {
+        std::optional<Args> result = ParseArgs(argc, argv);
+        if (!result.has_value()) {
+            std::cout << "Wrong amount of parameters" << std::endl;
+            std::cout << "Usage: radix <source notation> <destination notation> <value>" << std::endl;
+            return 1;
+        }
+        long decimalNumberView = StringToInt(result->value, result->sourceNotation);
         std::cout << IntToString(decimalNumberView, result->destinationNotation) << std::endl;
-	} catch (std::exception &err) {
-		std::cout << err.what() << std::endl;
-		return 1;
-	}
-	return 0;
+    } catch (std::exception &err) {
+        std::cout << err.what() << std::endl;
+        return 1;
+    }
+    return 0;
 }
 
 /* Functions */
 bool IsCapitalLetter(char ch) {
-	return (ch <= 'Z') && (ch >= 'A');
+    return (ch <= 'Z') && (ch >= 'A');
 }
 
 bool IsDigit(char ch) {
-	return (ch >= '0') && (ch <= '9');
+    return (ch >= '0') && (ch <= '9');
 }
 
 uint8_t CharToIntRadix(char ch, uint8_t radix) {
-	if (IsDigit(ch)) {
-		if (ch - '0' >= radix) {
-			throw std::logic_error("char is bigger than radix");
-		}
-		return ch - '0';
-	}
+    if (IsDigit(ch)) {
+        if (ch - '0' >= radix) {
+            throw std::logic_error("char is bigger than radix");
+        }
+        return ch - '0';
+    }
 
-	if (IsCapitalLetter(ch)) {
-		if (ch - 'A' + 10 >= radix) {
-			throw std::logic_error("char is bigger than radix");
-		}
-		return ch - 'A' + 10;
-	}
+    if (IsCapitalLetter(ch)) {
+        if (ch - 'A' + 10 >= radix) {
+            throw std::logic_error("char is bigger than radix");
+        }
+        return ch - 'A' + 10;
+    }
 
-	throw std::logic_error("can not covert char to int");
+    throw std::logic_error("can not covert char to int");
 }
 
 long StringToInt(const std::string &str, uint8_t radix) {
-	if ((radix > MAX_RADIX) || (radix < MIN_RADIX)) {
-		throw std::runtime_error("radix is not in correct range");
-	}
+    if ((radix > MAX_RADIX) || (radix < MIN_RADIX)) {
+        throw std::runtime_error("radix is not in correct range");
+    }
 
-	bool isNegative = false;
+    bool isNegative = false;
     if (str.length() > 0 && str[0] == '-') {
         isNegative = true;
     }
 
     long result = 0;
-	int offset = 0;
-	for (int i = (int) str.length() - 1; i >= 0; i--) {
-	    if (i == 0 && isNegative) {
-	        result *= -1;
+    int offset = 0;
+    for (int i = (int) str.length() - 1; i >= 0; i--) {
+        if (i == 0 && isNegative) {
+            result *= -1;
             continue;
-	    }
+        }
         result = result + ((long) pow(radix, offset)) * CharToIntRadix(str[i], radix);
         offset++;
-	}
+    }
 
-	return result;
+    return result;
 }
 
 char IntToCharRadix(uint8_t n, uint8_t radix) {
@@ -164,5 +164,5 @@ std::string IntToString(long n, uint8_t radix) {
     }
 
     std::reverse(result.begin(), result.end());
-	return result;
+    return result;
 }
