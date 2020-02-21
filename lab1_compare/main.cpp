eb3fe083db8a7b0ff91f0a0885be668100125a87
Kernel Mrex <24671528+KernelMrex@users.noreply.github.com>
2020-02-21 08:22:04 +0300

    Files formatted

--- lab1_compare/main.cpp
+++ lab1_compare/main.cpp
@@ -1,91 +1,110 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <optional>
 #include <stdexcept>
 
-struct Args {
-    std::string filePath1;
-    std::string filePath2;
+struct Args
+{
+	std::string filePath1;
+	std::string filePath2;
 };
 
-std::optional<Args> ParseArgs(int argc, char **argv) {
-    // Validate input values
-    if (argc != 3) {
-        return std::nullopt;
-    }
+std::optional<Args> ParseArgs(int argc, char** argv)
+{
+	// Validate input values
+	if (argc != 3)
+	{
+		return std::nullopt;
+	}
 
-    // Get filenames
-    return {(Args) {argv[1], argv[2]}};
+	// Get filenames
+	return { (Args){ argv[1], argv[2] } };
 }
 
-unsigned long CompareStreams(std::istream& in1, std::istream& in2) {
-    char bufferChar1, bufferChar2;
-    unsigned long amountOfNewLineChars = 1;
-    while (in1.get(bufferChar1).good() & in2.get(bufferChar2).good()) {
-        if (bufferChar1 != bufferChar2) {
-            return amountOfNewLineChars;
-        }
-        if (bufferChar1 == '\n') {
-            amountOfNewLineChars++;
-        }
-    }
+unsigned long CompareStreams(std::istream& in1, std::istream& in2)
+{
+	char bufferChar1, bufferChar2;
+	unsigned long amountOfNewLineChars = 1;
+	while (in1.get(bufferChar1).good() & in2.get(bufferChar2).good())
+	{
+		if (bufferChar1 != bufferChar2)
+		{
+			return amountOfNewLineChars;
+		}
+		if (bufferChar1 == '\n')
+		{
+			amountOfNewLineChars++;
+		}
+	}
 
-    // Checking EOF
-    if (!in1.eof() || !in2.eof()) {
-        return amountOfNewLineChars;
-    }
+	// Checking EOF
+	if (!in1.eof() || !in2.eof())
+	{
+		return amountOfNewLineChars;
+	}
 
-    // Checking errors while reading
-    if (in1.bad()) {
-        throw std::runtime_error("Error while reading first stream");
-    }
-    if (in2.bad()) {
-        throw std::runtime_error("Error while reading second stream");
-    }
-    return 0;
+	// Checking errors while reading
+	if (in1.bad())
+	{
+		throw std::runtime_error("Error while reading first stream");
+	}
+	if (in2.bad())
+	{
+		throw std::runtime_error("Error while reading second stream");
+	}
+	return 0;
 }
 
-unsigned long CompareFiles(const std::string& filePath1, const std::string& filePath2) {
-    // Check if input file exists
-    std::ifstream inputFile1;
-    inputFile1.open(filePath1);
-    if (!inputFile1.is_open()) {
-        throw std::runtime_error("Error '" + filePath1 + "' file not exists");
-    }
+unsigned long CompareFiles(const std::string& filePath1, const std::string& filePath2)
+{
+	// Check if input file exists
+	std::ifstream inputFile1;
+	inputFile1.open(filePath1);
+	if (!inputFile1.is_open())
+	{
+		throw std::runtime_error("Error '" + filePath1 + "' file not exists");
+	}
 
-    // Check if input file exists
-    std::ifstream inputFile2;
-    inputFile2.open(filePath2);
-    if (!inputFile2.is_open()) {
-        throw std::runtime_error("Error '" + filePath2 + "' file not exists");
-    }
+	// Check if input file exists
+	std::ifstream inputFile2;
+	inputFile2.open(filePath2);
+	if (!inputFile2.is_open())
+	{
+		throw std::runtime_error("Error '" + filePath2 + "' file not exists");
+	}
 
-    return CompareStreams(inputFile1, inputFile2);
+	return CompareStreams(inputFile1, inputFile2);
 }
 
-int main(int argc, char *argv[]) {
-    // Parse args
-    std::optional<Args> args = ParseArgs(argc, argv);
-    if (!args.has_value()) {
-        std::cout << "Invalid arguments amount. Usage compare.out <file1> <file2>" << std::endl;
-        return 1;
-    }
+int main(int argc, char* argv[])
+{
+	// Parse args
+	std::optional<Args> args = ParseArgs(argc, argv);
+	if (!args.has_value())
+	{
+		std::cout << "Invalid arguments amount. Usage compare.out <file1> <file2>" << std::endl;
+		return 1;
+	}
 
-    // Comparing files
-    unsigned long difLine;
-    try {
-        difLine = CompareFiles(args->filePath1, args->filePath2);
-    } catch (std::exception& err) {
-        std::cout << err.what() << std::endl;
-        return 1;
-    }
+	// Comparing files
+	unsigned long difLine;
+	try
+	{
+		difLine = CompareFiles(args->filePath1, args->filePath2);
+	}
+	catch (const std::exception& err)
+	{
+		std::cout << err.what() << std::endl;
+		return 1;
+	}
 
-    // Writing result
-    if (difLine) {
-        std::cout << "Files are different. Line number is " << difLine << std::endl;
-        return 1;
-    }
-    std::cout << "Files are equal" << std::endl;
+	// Writing result
+	if (difLine)
+	{
+		std::cout << "Files are different. Line number is " << difLine << std::endl;
+		return 1;
+	}
+	std::cout << "Files are equal" << std::endl;
 
-    return 0;
+	return 0;
 }
--- lab1_radix/main.cpp
+++ lab1_radix/main.cpp
@@ -1,8 +1,8 @@
+#include <algorithm>
 #include <cmath>
 #include <iostream>
 #include <optional>
 #include <string>
-#include <algorithm>
 
 #define MAX_RADIX 36
 #define MIN_RADIX 2
@@ -12,157 +12,188 @@ bool IsCapitalLetter(char ch);
 
 bool IsDigit(char ch);
 
-uint8_t CharToIntRadix(char ch, uint8_t radix);
+int CharToIntRadix(char ch, int radix);
 
-char IntToCharRadix(uint8_t n, uint8_t radix);
+char IntToCharRadix(int n, int radix);
 
-long StringToInt(const std::string &str, uint8_t radix);
+long StringToInt(const std::string& str, int radix);
 
-std::string IntToString(long n, uint8_t radix);
+std::string IntToString(long n, int radix);
 
 /* Main logic */
-struct Args {
-    std::uint8_t sourceNotation;
-    std::uint8_t destinationNotation;
-    std::string value;
+struct Args
+{
+	int sourceNotation;
+	int destinationNotation;
+	std::string value;
 };
 
-std::optional<Args> ParseArgs(int argc, char *argv[]) {
-    // Validate input values
-    if (argc != 4) {
-        return std::nullopt;
-    }
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+	// Validate input values
+	if (argc != 4)
+	{
+		return std::nullopt;
+	}
 
-    long sourceNotation = StringToInt(argv[1], 10);
-    if ((sourceNotation > MAX_RADIX) || (sourceNotation < MIN_RADIX)) {
-        throw std::runtime_error("bad range for source notation");
-    }
+	long sourceNotation = StringToInt(argv[1], 10);
+	if ((sourceNotation > MAX_RADIX) || (sourceNotation < MIN_RADIX))
+	{
+		throw std::runtime_error("bad range for source notation");
+	}
 
-    long destinationNotation = StringToInt(argv[2], 10);
-    if ((destinationNotation > MAX_RADIX) || (destinationNotation < MIN_RADIX)) {
-        throw std::runtime_error("bad range for destination notation");
-    }
+	long destinationNotation = StringToInt(argv[2], 10);
+	if ((destinationNotation > MAX_RADIX) || (destinationNotation < MIN_RADIX))
+	{
+		throw std::runtime_error("bad range for destination notation");
+	}
 
-    return {
-            (Args) {
-                    static_cast<uint8_t>(sourceNotation),
-                    static_cast<uint8_t>(destinationNotation),
-                    argv[3]
-            }
-    };
+	return {
+		(Args){
+			static_cast<int>(sourceNotation),
+			static_cast<int>(destinationNotation),
+			argv[3] }
+	};
 }
 
-int main(int argc, char *argv[]) {
-    try {
-        std::optional<Args> result = ParseArgs(argc, argv);
-        if (!result.has_value()) {
-            std::cout << "Wrong amount of parameters" << std::endl;
-            std::cout << "Usage: radix <source notation> <destination notation> <value>" << std::endl;
-            return 1;
-        }
-        long decimalNumberView = StringToInt(result->value, result->sourceNotation);
-        std::cout << IntToString(decimalNumberView, result->destinationNotation) << std::endl;
-    } catch (std::exception &err) {
-        std::cout << err.what() << std::endl;
-        return 1;
-    }
-    return 0;
+int main(int argc, char* argv[])
+{
+	try
+	{
+		std::optional<Args> result = ParseArgs(argc, argv);
+		if (!result.has_value())
+		{
+			std::cout << "Wrong amount of parameters" << std::endl;
+			std::cout << "Usage: radix <source notation> <destination notation> <value>" << std::endl;
+			return 1;
+		}
+		long number = StringToInt(result->value, result->sourceNotation);
+		std::cout << IntToString(number, result->destinationNotation) << std::endl;
+	}
+	catch (const std::exception& err)
+	{
+		std::cout << err.what() << std::endl;
+		return 1;
+	}
+	return 0;
 }
 
 /* Functions */
-bool IsCapitalLetter(char ch) {
-    return (ch <= 'Z') && (ch >= 'A');
+bool IsCapitalLetter(char ch)
+{
+	return (ch <= 'Z') && (ch >= 'A');
 }
 
-bool IsDigit(char ch) {
-    return (ch >= '0') && (ch <= '9');
+bool IsDigit(char ch)
+{
+	return (ch >= '0') && (ch <= '9');
 }
 
-uint8_t CharToIntRadix(char ch, uint8_t radix) {
-    if (IsDigit(ch)) {
-        if (ch - '0' >= radix) {
-            throw std::logic_error("char is bigger than radix");
-        }
-        return ch - '0';
-    }
+int CharToIntRadix(char ch, int radix)
+{
+	if (IsDigit(ch))
+	{
+		if (ch - '0' >= radix)
+		{
+			throw std::logic_error("char is bigger than radix");
+		}
+		return ch - '0';
+	}
 
-    if (IsCapitalLetter(ch)) {
-        if (ch - 'A' + 10 >= radix) {
-            throw std::logic_error("char is bigger than radix");
-        }
-        return ch - 'A' + 10;
-    }
+	if (IsCapitalLetter(ch))
+	{
+		if (ch - 'A' + 10 >= radix)
+		{
+			throw std::logic_error("char is bigger than radix");
+		}
+		return ch - 'A' + 10;
+	}
 
-    throw std::logic_error("can not covert char to int");
+	throw std::logic_error("can not covert char to int");
 }
 
-long StringToInt(const std::string &str, uint8_t radix) {
-    if ((radix > MAX_RADIX) || (radix < MIN_RADIX)) {
-        throw std::runtime_error("radix is not in correct range");
-    }
+long StringToInt(const std::string& str, int radix)
+{
+	if ((radix > MAX_RADIX) || (radix < MIN_RADIX))
+	{
+		throw std::runtime_error("radix is not in correct range");
+	}
 
-    bool isNegative = false;
-    if (str.length() > 0 && str[0] == '-') {
-        isNegative = true;
-    }
+	bool isNegative = false;
+	if (str.length() > 0 && str[0] == '-')
+	{
+		isNegative = true;
+	}
 
-    long result = 0;
-    int offset = 0;
-    for (int i = (int) str.length() - 1; i >= 0; i--) {
-        if (i == 0 && isNegative) {
-            result *= -1;
-            continue;
-        }
-        result = result + ((long) pow(radix, offset)) * CharToIntRadix(str[i], radix);
-        offset++;
-    }
+	long result = 0;
+	int offset = 0;
+	for (int i = (int)str.length() - 1; i >= 0; i--)
+	{
+		if (i == 0 && isNegative)
+		{
+			result *= -1;
+			continue;
+		}
+		result = result + ((long)pow(radix, offset)) * CharToIntRadix(str[i], radix);
+		offset++;
+	}
 
-    return result;
+	return result;
 }
 
-char IntToCharRadix(uint8_t n, uint8_t radix) {
-    if ((radix > MAX_RADIX) || (radix < MIN_RADIX)) {
-        throw std::runtime_error("radix is not in correct range");
-    }
+char IntToCharRadix(int n, int radix)
+{
+	if ((radix > MAX_RADIX) || (radix < MIN_RADIX))
+	{
+		throw std::runtime_error("radix is not in correct range");
+	}
 
-    if (n >= radix) {
-        throw std::runtime_error("number is bigger than radix");
-    }
+	if (n >= radix)
+	{
+		throw std::runtime_error("number is bigger than radix");
+	}
 
-    if ((n >= 0) && (n < 9)) {
-        return static_cast<char>('0' + n);
-    }
+	if ((n >= 0) && (n < 9))
+	{
+		return static_cast<char>('0' + n);
+	}
 
-    if ((n >= 10) && (n < 36)) {
-        return static_cast<char>('A' + n);
-    }
+	if ((n >= 10) && (n < 36))
+	{
+		return static_cast<char>('A' + n);
+	}
 
-    throw std::runtime_error("number is not in correct range");
+	throw std::runtime_error("number is not in correct range");
 }
 
-std::string IntToString(long n, uint8_t radix) {
-    if (n == 0) {
-        return "0";
-    }
+std::string IntToString(long n, int radix)
+{
+	if (n == 0)
+	{
+		return "0";
+	}
 
-    bool isNegative = false;
-    if (n < 0) {
-        n = -n;
-        isNegative = true;
-    }
+	bool isNegative = false;
+	if (n < 0)
+	{
+		n = -n;
+		isNegative = true;
+	}
 
-    std::string result = "";
-    long divResult;
-    while (n > 0) {
-        divResult = (n / radix);
-        result += IntToCharRadix(n - (divResult * radix), radix);
-        n = divResult;
-    }
+	std::string result = "";
+	long divResult;
+	while (n > 0)
+	{
+		divResult = (n / radix);
+		result += IntToCharRadix(n - (divResult * radix), radix);
+		n = divResult;
+	}
 
-    if (isNegative) {
-        result += '-';
-    }
+	if (isNegative)
+	{
+		result += '-';
+	}
 
-    std::reverse(result.begin(), result.end());
-    return result;
+	std::reverse(result.begin(), result.end());
+	return result;
 }
