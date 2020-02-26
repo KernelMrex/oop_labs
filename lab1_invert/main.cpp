d9f24a2fe03ebeb4b22ea82052c3bdcde42252dd
Kernel Mrex <24671528+KernelMrex@users.noreply.github.com>
2020-02-26 19:19:39 +0300

    Added

--- lab1_invert/main.cpp
+++ lab1_invert/main.cpp
@@ -2,24 +2,29 @@
 #include <optional>
 #include <fstream>
 
-#define MATRIX_SIZE 3
+#define MATRIX_3x3_SIZE 3
+#define MATRIX_2x2_SIZE 2
 
 struct Args {
     std::string filepath;
 };
 
-typedef double Matrix[MATRIX_SIZE][MATRIX_SIZE];
+typedef double Matrix3x3[MATRIX_3x3_SIZE][MATRIX_3x3_SIZE];
+
+typedef double Matrix2x2[MATRIX_2x2_SIZE][MATRIX_2x2_SIZE];
 
 
 std::optional<Args> ParseArgs(int argc, char *argv[]);
 
-void ReadMatrixFromFile(const std::string &filepath, Matrix &matrix);
+void ReadMatrixFromFile(const std::string &filepath, Matrix3x3 &matrix);
 
-void ReadMatrix(std::istream &in, Matrix &matrix);
+void ReadMatrix(std::istream &in, Matrix3x3 &matrix);
 
-long double CalcMatrixDeterminant(Matrix &matrix);
+long double CalcMatrixDeterminant(Matrix3x3 &matrix);
 
-void PrintMatrix(std::ostream &out, Matrix &matrix);
+void PrintMatrix(std::ostream &out, Matrix3x3 &matrix);
+
+void CalcAlgebComplMatrix(const Matrix3x3 &matrix, Matrix3x3 &minorsMatrix);
 
 int main(int argc, char *argv[]) {
     // Parsing arguments
@@ -31,7 +36,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Reading matrix
-    Matrix matrix = {};
+    Matrix3x3 matrix = {};
     try {
         ReadMatrixFromFile(args->filepath, matrix);
     } catch (std::exception &err) {
@@ -39,14 +44,23 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Calculating invert matrix
+    // Calculating matrix determinant
     long double matrixDeterminant = CalcMatrixDeterminant(matrix);
     if (matrixDeterminant == 0) {
         std::cout << "Invert matrix not exists" << std::endl;
         return 1;
     }
 
+    // Calculating minors
+    Matrix3x3 minorsMatrix = {};
+    CalcAlgebComplMatrix(matrix, minorsMatrix);
+
     // TODO: ...
+    PrintMatrix(std::cout, matrix);
+
+    std::cout << std::endl;
+
+    PrintMatrix(std::cout, minorsMatrix);
 
     return 0;
 }
@@ -59,7 +73,7 @@ std::optional<Args> ParseArgs(int argc, char *argv[]) {
     return {(Args) {argv[1]}};
 }
 
-void ReadMatrixFromFile(const std::string &filepath, Matrix &matrix) {
+void ReadMatrixFromFile(const std::string &filepath, Matrix3x3 &matrix) {
     std::ifstream file;
 
     std::cout << filepath << std::endl;
@@ -71,17 +85,17 @@ void ReadMatrixFromFile(const std::string &filepath, Matrix &matrix) {
     ReadMatrix(file, matrix);
 }
 
-void ReadMatrix(std::istream &in, Matrix &matrix) {
+void ReadMatrix(std::istream &in, Matrix3x3 &matrix) {
     for (auto &matrixRow : matrix) {
         for (double &matrixField : matrixRow) {
             if (!(in >> matrixField)) {
-                throw std::runtime_error("");
+                throw std::runtime_error("error while reading matrix");
             }
         }
     }
 }
 
-long double CalcMatrixDeterminant(Matrix &matrix) {
+long double CalcMatrixDeterminant(Matrix3x3 &matrix) {
     return
             matrix[0][0] * matrix[1][1] * matrix[2][2]
             + matrix[0][2] * matrix[1][0] * matrix[2][1]
@@ -91,11 +105,21 @@ long double CalcMatrixDeterminant(Matrix &matrix) {
             - matrix[0][1] * matrix[1][0] * matrix[2][2];
 }
 
-void PrintMatrix(std::ostream &out, Matrix &matrix) {
+void PrintMatrix(std::ostream &out, Matrix3x3 &matrix) {
     for (auto &matrixRow : matrix) {
         for (double &matrixField : matrixRow) {
             out << matrixField << " ";
         }
         out << std::endl;
+    }
+}
+
+void CalcAlgebComplMatrix(const Matrix3x3 &matrix, Matrix3x3 &minorsMatrix) {
+    for (int i = 0; i < MATRIX_3x3_SIZE; i++) {
+        for (int j = 0; j < MATRIX_3x3_SIZE; j++) {
+            double minor = matrix[i][j] * matrix[(i + 1) % 3][(j + 1) % 3]
+                                - matrix[(i + 1) % 3][j] * matrix[i][(j + 1) % 3];
+            minorsMatrix[(i + 2) % 3][(j + 2) % 3] = minor;
+        }
     }
 }
