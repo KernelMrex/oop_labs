42b6ec5c5ca6973b53629251e62bdcebfd520160
Kernel Mrex <24671528+KernelMrex@users.noreply.github.com>
2020-02-28 00:09:04 +0300

    Added SafeSubtraction and SafeAddition

--- lab1_invert/safe_operations.cpp
+++ lab1_invert/safe_operations.cpp
@@ -22,9 +22,33 @@ double SafeMultiply(double x, double y) {
 }
 
 double SafeAddition(double x, double y) {
+    if (x == std::numeric_limits<double>::min()) {
+        throw std::runtime_error("addition overflow: x");
+    }
+
+    if (y == std::numeric_limits<double>::min()) {
+        throw std::runtime_error("addition overflow: y");
+    }
+
+    if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() - (y > 0 ? y : -y)) {
+        throw std::runtime_error("addition overflow");
+    }
+
     return x + y;
 }
 
 double SafeSubtraction(double x, double y) {
+    if (x == std::numeric_limits<double>::min()) {
+        throw std::runtime_error("addition subtraction: x");
+    }
+
+    if (y == std::numeric_limits<double>::min()) {
+        throw std::runtime_error("addition subtraction: y");
+    }
+
+    if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() + (y > 0 ? y : -y)) {
+        throw std::runtime_error("addition subtraction");
+    }
+
     return x - y;
 }
