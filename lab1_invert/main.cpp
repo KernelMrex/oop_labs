e8aa8b165443bb64225957eff65673406204144c
Kernel Mrex <24671528+KernelMrex@users.noreply.github.com>
2020-02-28 08:15:25 +0300

    Formatted

--- lab1_invert/main.cpp
+++ lab1_invert/main.cpp
@@ -1,17 +1,18 @@
+#include "safe_operations.h"
+#include <fstream>
 #include <iostream>
 #include <optional>
-#include <fstream>
-#include "safe_operations.h"
 
 #define MATRIX_3x3_SIZE 3
 
-struct Args {
-    std::string filepath;
+struct Args
+{
+	std::string filepath;
 };
 
 typedef double Matrix3x3[MATRIX_3x3_SIZE][MATRIX_3x3_SIZE];
 
-std::optional<Args> ParseArgs(int argc, char *argv[]);
+std::optional<Args> ParseArgs(int argc, char* argv[]);
 
 void ReadMatrixFromFile(const std::string& filepath, Matrix3x3& matrix);
 
@@ -29,140 +30,163 @@ void CalcTransposedMatrix(const Matrix3x3& matrix, Matrix3x3& transposedMatrix);
 
 void CalcInvertMatrix(const Matrix3x3& matrix, Matrix3x3& invertMatrix);
 
-int main(int argc, char *argv[]) {
-    // Parsing arguments
-    auto args = ParseArgs(argc, argv);
-    if (!args.has_value()) {
-        std::cout << "Error: wrong amount of arguments" << std::endl;
-        std::cout << "Usage: invert <matrix file path>" << std::endl;
-        return 1;
-    }
+int main(int argc, char* argv[])
+{
+	// Parsing arguments
+	auto args = ParseArgs(argc, argv);
+	if (!args.has_value())
+	{
+		std::cout << "Error: wrong amount of arguments" << std::endl;
+		std::cout << "Usage: invert <matrix file path>" << std::endl;
+		return 1;
+	}
 
-    // Reading matrix
-    Matrix3x3 matrix = {};
-    try {
-        ReadMatrixFromFile(args->filepath, matrix);
-    } catch (std::exception& err) {
-        std::cout << err.what() << std::endl;
-        return 1;
-    }
+	// Reading matrix
+	Matrix3x3 matrix = {};
+	try
+	{
+		ReadMatrixFromFile(args->filepath, matrix);
+	}
+	catch (std::exception& err)
+	{
+		std::cout << err.what() << std::endl;
+		return 1;
+	}
 
-    // Calculating invert matrix
-    Matrix3x3 invertMatrix = {};
-    try {
-        CalcInvertMatrix(matrix, invertMatrix);
+	// Calculating invert matrix
+	Matrix3x3 invertMatrix = {};
+	try
+	{
+		CalcInvertMatrix(matrix, invertMatrix);
+	}
+	catch (std::exception& err)
+	{
+		std::cout << err.what() << std::endl;
+		return 1;
+	}
 
-    } catch (std::exception& err) {
-        std::cout << err.what() << std::endl;
-        return 1;
-    }
+	// Writing matrix
+	PrintMatrix(std::cout, invertMatrix);
 
-    // Writing matrix
-    PrintMatrix(std::cout, invertMatrix);
-
-    return 0;
+	return 0;
 }
 
-std::optional<Args> ParseArgs(int argc, char *argv[]) {
-    if (argc != 2) {
-        return std::nullopt;
-    }
-    return {(Args) {argv[1]}};
+std::optional<Args> ParseArgs(int argc, char* argv[])
+{
+	if (argc != 2)
+	{
+		return std::nullopt;
+	}
+	return { (Args){ argv[1] } };
 }
 
-void ReadMatrixFromFile(const std::string& filepath, Matrix3x3& matrix) {
-    std::ifstream file;
-    std::cout << filepath << std::endl;
-    file.open(filepath, std::ios::in);
-    if (!file.is_open()) {
-        throw std::runtime_error("cannot open file '" + filepath + "'");
-    }
-    ReadMatrix(file, matrix);
+void ReadMatrixFromFile(const std::string& filepath, Matrix3x3& matrix)
+{
+	std::ifstream file;
+	std::cout << filepath << std::endl;
+	file.open(filepath, std::ios::in);
+	if (!file.is_open())
+	{
+		throw std::runtime_error("cannot open file '" + filepath + "'");
+	}
+	ReadMatrix(file, matrix);
 }
 
-void ReadMatrix(std::istream& in, Matrix3x3& matrix) {
-    for (auto& matrixRow : matrix) {
-        for (double& matrixField : matrixRow) {
-            if (!(in >> matrixField)) {
-                throw std::runtime_error("error while reading matrix");
-            }
-        }
-    }
+void ReadMatrix(std::istream& in, Matrix3x3& matrix)
+{
+	for (auto& matrixRow : matrix)
+	{
+		for (double& matrixField : matrixRow)
+		{
+			if (!(in >> matrixField))
+			{
+				throw std::runtime_error("error while reading matrix");
+			}
+		}
+	}
 }
 
-void PrintMatrix(std::ostream& out, Matrix3x3& matrix) {
-    for (auto& matrixRow : matrix) {
-        for (double& matrixField : matrixRow) {
-            out << matrixField << " ";
-        }
-        out << std::endl;
-    }
+void PrintMatrix(std::ostream& out, Matrix3x3& matrix)
+{
+	for (auto& matrixRow : matrix)
+	{
+		for (double& matrixField : matrixRow)
+		{
+			out << matrixField << " ";
+		}
+		out << std::endl;
+	}
 }
 
-void DivideMatrix(const Matrix3x3& matrix, double divisor, Matrix3x3& resultMatrix) {
-    for (int i = 0; i < MATRIX_3x3_SIZE; i++) {
-        for (int j = 0; j < MATRIX_3x3_SIZE; j++) {
-            resultMatrix[i][j] = SafeDivision(matrix[i][j], divisor);
-        }
-    }
+void DivideMatrix(const Matrix3x3& matrix, double divisor, Matrix3x3& resultMatrix)
+{
+	for (int i = 0; i < MATRIX_3x3_SIZE; i++)
+	{
+		for (int j = 0; j < MATRIX_3x3_SIZE; j++)
+		{
+			resultMatrix[i][j] = SafeDivision(matrix[i][j], divisor);
+		}
+	}
 }
 
-double CalcMatrixDeterminant(const Matrix3x3& matrix) {
-    return
-            SafeSubtraction(
-                    SafeAddition(
-                            SafeAddition(
-                                    SafeMultiply(SafeMultiply(matrix[0][0], matrix[1][1]), matrix[2][2]),
-                                    SafeMultiply(SafeMultiply(matrix[0][2], matrix[1][0]), matrix[2][1])
-                            ),
-                            SafeMultiply(SafeMultiply(matrix[0][1], matrix[1][2]), matrix[2][0])
-                    ),
-                    SafeAddition(
-                            SafeAddition(
-                                    SafeMultiply(SafeMultiply(matrix[0][2], matrix[1][1]), matrix[2][0]),
-                                    SafeMultiply(SafeMultiply(matrix[0][0], matrix[1][2]), matrix[2][1])
-                            ),
-                            SafeMultiply(SafeMultiply(matrix[0][1], matrix[1][0]), matrix[2][2])
-                    )
-            );
+double CalcMatrixDeterminant(const Matrix3x3& matrix)
+{
+	return SafeSubtraction(
+		SafeAddition(
+			SafeAddition(
+				SafeMultiply(SafeMultiply(matrix[0][0], matrix[1][1]), matrix[2][2]),
+				SafeMultiply(SafeMultiply(matrix[0][2], matrix[1][0]), matrix[2][1])),
+			SafeMultiply(SafeMultiply(matrix[0][1], matrix[1][2]), matrix[2][0])),
+		SafeAddition(
+			SafeAddition(
+				SafeMultiply(SafeMultiply(matrix[0][2], matrix[1][1]), matrix[2][0]),
+				SafeMultiply(SafeMultiply(matrix[0][0], matrix[1][2]), matrix[2][1])),
+			SafeMultiply(SafeMultiply(matrix[0][1], matrix[1][0]), matrix[2][2])));
 }
 
-void CalcAlgebraicComplementMatrix(const Matrix3x3& matrix, Matrix3x3& algebraicComplementMatrix) {
-    for (int i = 0; i < MATRIX_3x3_SIZE; i++) {
-        for (int j = 0; j < MATRIX_3x3_SIZE; j++) {
-            double minor = SafeSubtraction(
-                    SafeMultiply(matrix[i][j], matrix[(i + 1) % 3][(j + 1) % 3]),
-                    SafeMultiply(matrix[(i + 1) % 3][j], matrix[i][(j + 1) % 3])
-            );
-            algebraicComplementMatrix[(i + 2) % 3][(j + 2) % 3] = minor;
-        }
-    }
+void CalcAlgebraicComplementMatrix(const Matrix3x3& matrix, Matrix3x3& algebraicComplementMatrix)
+{
+	for (int i = 0; i < MATRIX_3x3_SIZE; i++)
+	{
+		for (int j = 0; j < MATRIX_3x3_SIZE; j++)
+		{
+			double minor = SafeSubtraction(
+				SafeMultiply(matrix[i][j], matrix[(i + 1) % 3][(j + 1) % 3]),
+				SafeMultiply(matrix[(i + 1) % 3][j], matrix[i][(j + 1) % 3]));
+			algebraicComplementMatrix[(i + 2) % 3][(j + 2) % 3] = minor;
+		}
+	}
 }
 
-void CalcTransposedMatrix(const Matrix3x3& matrix, Matrix3x3& transposedMatrix) {
-    for (int i = 0; i < MATRIX_3x3_SIZE; i++) {
-        for (int j = i; j < MATRIX_3x3_SIZE; j++) {
-            transposedMatrix[j][i] = matrix[i][j];
-            transposedMatrix[i][j] = matrix[j][i];
-        }
-    }
+void CalcTransposedMatrix(const Matrix3x3& matrix, Matrix3x3& transposedMatrix)
+{
+	for (int i = 0; i < MATRIX_3x3_SIZE; i++)
+	{
+		for (int j = i; j < MATRIX_3x3_SIZE; j++)
+		{
+			transposedMatrix[j][i] = matrix[i][j];
+			transposedMatrix[i][j] = matrix[j][i];
+		}
+	}
 }
 
-void CalcInvertMatrix(const Matrix3x3& matrix, Matrix3x3& invertMatrix) {
-    // Calculating matrix determinant
-    double matrixDeterminant = CalcMatrixDeterminant(matrix);
-    if (matrixDeterminant == 0) {
-        throw std::logic_error("Invert matrix not exists");
-    }
+void CalcInvertMatrix(const Matrix3x3& matrix, Matrix3x3& invertMatrix)
+{
+	// Calculating matrix determinant
+	double matrixDeterminant = CalcMatrixDeterminant(matrix);
+	if (matrixDeterminant == 0)
+	{
+		throw std::logic_error("Invert matrix not exists");
+	}
 
-    // Calculating algebraic complement matrix
-    Matrix3x3 algebraicComplementMatrix = {};
-    CalcAlgebraicComplementMatrix(matrix, algebraicComplementMatrix);
+	// Calculating algebraic complement matrix
+	Matrix3x3 algebraicComplementMatrix = {};
+	CalcAlgebraicComplementMatrix(matrix, algebraicComplementMatrix);
 
-    // Calculating transposed matrix
-    Matrix3x3 transposedMatrix = {};
-    CalcTransposedMatrix(algebraicComplementMatrix, transposedMatrix);
+	// Calculating transposed matrix
+	Matrix3x3 transposedMatrix = {};
+	CalcTransposedMatrix(algebraicComplementMatrix, transposedMatrix);
 
-    // Calculating result
-    DivideMatrix(transposedMatrix, matrixDeterminant, invertMatrix);
+	// Calculating result
+	DivideMatrix(transposedMatrix, matrixDeterminant, invertMatrix);
 }
--- lab1_invert/safe_operations.cpp
+++ lab1_invert/safe_operations.cpp
@@ -1,70 +1,87 @@
 #include <limits>
 #include <stdexcept>
 
-double SafeMultiply(double x, double y) {
-    if (y == 0) {
-        return 0;
-    }
+double SafeMultiply(double x, double y)
+{
+	if (y == 0)
+	{
+		return 0;
+	}
 
-    if (x == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("multiply overflow: x");
-    }
+	if (x == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("multiply overflow: x");
+	}
 
-    if (y == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("multiply overflow: y");
-    }
+	if (y == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("multiply overflow: y");
+	}
 
-    if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() / (y > 0 ? y : -y)) {
-        throw std::runtime_error("multiply overflow");
-    }
+	if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() / (y > 0 ? y : -y))
+	{
+		throw std::runtime_error("multiply overflow");
+	}
 
-    return x * y;
+	return x * y;
 }
 
-double SafeAddition(double x, double y) {
-    if (x == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("addition overflow: x");
-    }
+double SafeAddition(double x, double y)
+{
+	if (x == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("addition overflow: x");
+	}
 
-    if (y == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("addition overflow: y");
-    }
+	if (y == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("addition overflow: y");
+	}
 
-    if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() - (y > 0 ? y : -y)) {
-        throw std::runtime_error("addition overflow");
-    }
+	if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() - (y > 0 ? y : -y))
+	{
+		throw std::runtime_error("addition overflow");
+	}
 
-    return x + y;
+	return x + y;
 }
 
-double SafeSubtraction(double x, double y) {
-    if (x == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("addition subtraction: x");
-    }
+double SafeSubtraction(double x, double y)
+{
+	if (x == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("addition subtraction: x");
+	}
 
-    if (y == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("addition subtraction: y");
-    }
+	if (y == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("addition subtraction: y");
+	}
 
-    if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() + (y > 0 ? y : -y)) {
-        throw std::runtime_error("addition subtraction");
-    }
+	if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() + (y > 0 ? y : -y))
+	{
+		throw std::runtime_error("addition subtraction");
+	}
 
-    return x - y;
+	return x - y;
 }
 
-double SafeDivision(double x, double y) {
-    if (x == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("addition division: x");
-    }
+double SafeDivision(double x, double y)
+{
+	if (x == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("addition division: x");
+	}
 
-    if (y == std::numeric_limits<double>::min()) {
-        throw std::runtime_error("addition division: y");
-    }
+	if (y == std::numeric_limits<double>::min())
+	{
+		throw std::runtime_error("addition division: y");
+	}
 
-    if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() * (y > 0 ? y : -y)) {
-        throw std::runtime_error("addition division");
-    }
+	if ((x > 0 ? x : -x) > std::numeric_limits<double>::max() * (y > 0 ? y : -y))
+	{
+		throw std::runtime_error("addition division");
+	}
 
-    return x / y;
+	return x / y;
 }
