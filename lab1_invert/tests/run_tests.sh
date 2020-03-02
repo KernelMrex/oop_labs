4eba15be30b1b9fedd6c74175681be16804d0a87
Kernel Mrex <24671528+KernelMrex@users.noreply.github.com>
2020-03-02 22:01:22 +0300

    Refactored code: CalcInvertMatrix now return std::optional<Matrix3x3>

--- lab1_invert/tests/run_tests.sh
+++ lab1_invert/tests/run_tests.sh
@@ -11,20 +11,23 @@ cp ../cmake-build-debug/$EXEC_NAME .
 for entry in *.test
 do
   args=$entry
-  result=$(./$EXEC_NAME "$args")
-
+  ./$EXEC_NAME "$args" > test_1.tmp
   prefix="${entry:0:-5}"
 
-  if [ "$result" == "$(cat "$prefix".test.expected)" ]; then
+  diff=$(cmp "test_1.tmp" "$prefix".test.expected)
+
+  if [ "$diff" == "" ]; then
     printf "Test %s done!\n" "${entry:0:-5}"
   else
     printf "\n"
     printf "Test %s has failed\n" "${entry:0:-5}"
     printf "Arguments: %s\n" "$args"
-    printf "Output: %s\n" "$result"
+    printf "Output: %s\n" "$(cat test_1.tmp)"
     printf "Expected: %s\n" "$(cat "$prefix".test.expected)"
+    printf "Comment: %s\n" "$diff"
     printf "\n"
   fi
 done
 
+rm test_1.tmp
 rm $EXEC_NAME
