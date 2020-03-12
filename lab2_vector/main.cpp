03b057c327fab40c7e973f9641d9f7d383b4254b
Kernel Mrex <24671528+KernelMrex@users.noreply.github.com>
2020-03-12 23:57:27 +0300

    lab2_vector done!

---
+++ lab2_vector/main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <optional>
+#include <sstream>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+std::optional<std::vector<double>> ReadVector(std::istream& input);
+
+void PrintVector(const std::vector<double>& vector, std::ostream& output);
+
+std::vector<double> GetMinItemsInVector(std::vector<double> vector, int minItemsAmount);
+
+std::vector<double> AddNumToEachItem(const std::vector<double>& vector, double num);
+
+std::vector<double> AddToEachItemAverageOf3MinNumbers(const std::vector<double>& vector);
+
+int main() {
+    auto vector = ReadVector(std::cin);
+    if (!vector.has_value()) {
+        std::cout << "Error while reading vector" << std::endl;
+        return 1;
+    }
+
+    auto resultVector = AddToEachItemAverageOf3MinNumbers(vector.value());
+
+    PrintVector(resultVector, std::cout);
+    return 0;
+}
+
+std::optional<std::vector<double>> ReadVector(std::istream& input) {
+    // Get first line of input
+    std::string inputString;
+    std::getline(input, inputString);
+    std::istringstream stringStream(inputString);
+
+    // Reading vector from first string
+    std::vector<double> vector;
+    double num;
+    while (stringStream >> num) {
+        vector.push_back(num);
+    }
+
+    if (!stringStream.eof() || stringStream.bad()) {
+        return std::nullopt;
+    }
+
+    return {vector};
+}
+
+void PrintVector(const std::vector<double>& vector, std::ostream& output) {
+    for (auto iterator = vector.begin(); iterator != vector.end(); std::advance(iterator, 1)) {
+        output << *iterator << " ";
+    }
+    output << std::endl;
+}
+
+std::vector<double> GetMinItemsInVector(std::vector<double> vector, int minItemsAmount) {
+    std::vector<double> result;
+    for (int i = 0; i < minItemsAmount; i++) {
+        if (vector.empty()) {
+            return vector;
+        }
+        auto minItem = std::min_element(vector.begin(), vector.end());
+        result.push_back(*minItem);
+        vector.erase(minItem);
+    }
+    return result;
+}
+
+std::vector<double> AddNumToEachItem(const std::vector<double>& vector, double num) {
+    std::vector<double> result = vector;
+    for (auto iterator = result.begin(); iterator != result.end(); std::advance(iterator, 1)) {
+        *iterator += num;
+    }
+    return result;
+}
+
+std::vector<double> AddToEachItemAverageOf3MinNumbers(const std::vector<double>& vector) {
+    auto minItems = GetMinItemsInVector(vector, 3);
+    auto average = std::round((std::accumulate(minItems.begin(), minItems.end(), 0.0) / 3) * 1000) / 1000;
+    return AddNumToEachItem(vector, average);
+}
---
+++ lab2_vector/tests/run_tests.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+#
+#  Auto tests by KernelMrex
+#
+
+EXEC_NAME="vector"
+
+cp ../cmake-build-debug/$EXEC_NAME .
+
+for entry in *.test.args; do
+  prefix="${entry:0:-10}"
+  args=($(cat "$entry"))
+
+  testType=${args[0]} || ""
+  inputFile=${args[1]} || ""
+  outputFile=${args[2]} || ""
+
+  fileToCompare=""
+  if [ "$testType" == "bad" ]; then
+    ./$EXEC_NAME $inputFile $outputFile >program_result.tmp
+    fileToCompare="program_result.tmp"
+  elif [ "$testType" == "good" ]; then
+    ./$EXEC_NAME "$inputFile" "$outputFile"
+    fileToCompare="$outputFile"
+  elif [ "$testType" == "std_input" ]; then
+    ./$EXEC_NAME < "$prefix.test.input" > program_result.tmp
+    fileToCompare="program_result.tmp"
+  elif [ "$testType" == "not_implemented" ]; then
+    printf "Skipping '%s' test... Reason: Not implemented yet!\n" "$prefix"
+    continue
+  fi
+
+  if cmp "$fileToCompare" "$prefix".test.expected; then
+    printf "Test %s done!\n" "$prefix"
+  else
+    printf "\n"
+    printf "Test %s has failed\n" "$prefix"
+    printf "Arguments: %s %s\n" "${args[1]}" "${args[2]}"
+    printf "Output: %s\n" "$(cat "$fileToCompare")"
+    printf "Expected: %s\n" "$(cat "$prefix".test.expected)"
+    printf "\n"
+  fi
+  #rm "$fileToCompare"
+done
+
+rm $EXEC_NAME
