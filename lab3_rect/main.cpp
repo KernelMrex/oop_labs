fec43157e19b134557d75d8a510900d4c6c13fe3
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2020-04-04 16:42:24 +0300

    Added CL arguments parser function

--- lab3_rect/main.cpp
+++ lab3_rect/main.cpp
@@ -1,10 +1,39 @@
 #include "CRectangle.h"
 #include <iostream>
+#include <optional>
 
-int main()
+struct Args
 {
-	CRectangle rect{ 1, 1, 3, 4 };
-	std::cout << rect.GetRight() << std::endl;
+	std::string InputFile;
+	std::string OutputFile;
+};
+
+std::optional<Args> GetArgs(int argc, char* argv[]);
+
+int main(int argc, char* argv[])
+{
+	auto args = GetArgs(argc, argv);
+	if (!args.has_value())
+	{
+		std::cout << "Bad arguments. Usage: rect <input file> <output file>";
+		return 1;
+	}
+
+	std::cout << "In: " << args->InputFile << std::endl
+			  << " Out: " << args->OutputFile << std::endl;
 
 	return 0;
+}
+
+std::optional<Args> GetArgs(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		return std::nullopt;
+	}
+	return (std::optional<Args>){
+		(Args){
+			argv[1],
+			argv[2] }
+	};
 }
