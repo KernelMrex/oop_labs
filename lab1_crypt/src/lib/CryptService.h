88aa8b69260d15b7f3e4b9242a03f13cacd6a8b8
KernelMrex <24671528+KernelMrex@users.noreply.github.com>
2022-02-28 01:56:56 +0300

    lab1_crypt добавил CryptService::Encode

---
+++ lab1_crypt/src/lib/CryptService.h
@@ -0,0 +1,53 @@
+#ifndef CRYPT_SERVICE_H
+#define CRYPT_SERVICE_H
+
+#include <iostream>
+#include <utility>
+
+class CryptService
+{
+public:
+	bool static Encrypt(std::istream& in, std::ostream& out, unsigned char keyByte)
+	{
+		unsigned char inByte, outByte;
+
+		while ((in >> inByte) && out)
+		{
+			outByte = inByte ^ keyByte;
+
+			out << (char) (
+				GetAndMoveBit(outByte, 7, 5)
+				| GetAndMoveBit(outByte, 6, 1)
+				| GetAndMoveBit(outByte, 5, 0)
+				| GetAndMoveBit(outByte, 4, 7)
+				| GetAndMoveBit(outByte, 3, 6)
+				| GetAndMoveBit(outByte, 2, 4)
+				| GetAndMoveBit(outByte, 1, 3)
+				| GetAndMoveBit(outByte, 0, 2)
+			);
+		}
+
+		return !(!in.eof() && !in.good() || !out);
+	}
+
+private:
+	/**
+	 * @param in source byte
+	 * @param from bit position in byte 0..7
+	 * @param to bit position in byte 0..7
+	 * @return byte with all 0 bits except one bit set to 1 in 'to' position
+	 */
+	static unsigned char GetAndMoveBit(unsigned char in, unsigned char from, unsigned char to)
+	{
+		if (from >= 8 || to >= 8)
+		{
+			throw std::invalid_argument("GetAndMoveBit 'from' and 'to' arguments must be in range [0..7]");
+		}
+
+		unsigned char res = (in & ((unsigned char) 1 << from));
+
+		return (from > to) ? (res >> (from  - to)) : (res << (to - from));
+	}
+};
+
+#endif // CRYPT_SERVICE_H
---
+++ lab1_crypt/test/unit/lib/CryptServiceTest.cpp
@@ -0,0 +1,18 @@
+#include <sstream>
+#include "../../../../catch.hpp"
+#include "../../../src/lib/CryptService.h"
+
+TEST_CASE("CryptService class must Encrypt or Decrypt", "[crypt_service]")
+{
+	SECTION("Encrypting with key")
+	{
+		std::stringstream iss, oss, res;
+
+		iss << (char) 0b00000000 << (char) 0b11111111 << (char) 0b00110011 << (char) 0b11001100;
+		res << (char) 0b01001011 << (char) 0b10110100 << (char) 0b11000110 << (char) 0b00111001;
+
+		CryptService::Encrypt(iss, oss, 0b01101010);
+
+		REQUIRE(oss.str() == res.str());
+	}
+}
